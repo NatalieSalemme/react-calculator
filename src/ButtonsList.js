35b3a11fe40b0df46e268963f6c9e1a6d0081261
Natalie Salemme <NatalieSalemme@gmail.com>
2018-11-10 12:20:00 -0800

    creating ButtonsList component to store buttons removing outline and adding active state for buttons

---
+++ src/ButtonsList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ButtonsList = props => (
+  <div class="buttonsList">
+
+  <input type="button" id="all-clear" class="compute btn" value="AC" />
+  <input type="button" id="clear-sign" class="compute btn" value="C" />
+  <input type="button" id="pos-or-neg" class="compute btn" value="+/-" />
+  <input type="button" id="division-sign" class="compute btn" value="÷" />
+  <input type="button" id="one" class="btn" value="1" />
+  <input type="button" id="two" class="btn" value="2" />
+  <input type="button" id="three" class="btn" value="3" />
+  <input type="button" id="multiplication-sign" class="compute btn" value="x" />
+  <input type="button" id="four" class="btn" value="4" />
+  <input type="button" id="five" class="btn" value="5" />
+  <input type="button" id="six" class="btn" value="6" />
+  <input type="button" id="minus-sign" class="compute btn" value="-" />
+  <input type="button" id="seven" class="btn" value="7" />
+  <input type="button" id="eight" class="btn" value="8" />
+  <input type="button" id="nine" class="btn" value="9" />
+  <input type="button" id="plus-sign" class="compute btn" value="+" />
+  <input type="button" id="zero" class="btn" value="0" />
+  <input type="button" id="decimal-sign" class="compute btn" value="." />
+  <input type="button" id="equals-sign" class="btn" value="=" />
+    </div>
+);
+
+export default ButtonsList;
