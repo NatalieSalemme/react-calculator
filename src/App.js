5eae192d479cd6d4c9e3984b4d7d6bfc7456a2b0
Natalie Salemme <NatalieSalemme@gmail.com>
2018-11-13 21:02:31 -0800

    adding functionality so if there is an error, user is only able to click clear or all clear to reset calculator

--- src/App.js
+++ src/App.js
@@ -4,30 +4,37 @@ import ButtonsList from './ButtonsList';
 
 class App extends Component {
   state = {
-    blackBox: []
+    blackBox: [],
+    error: false
   }
   handleClick = (e) => {
-    if(e === 'x') {
-      e = '*'
-    } else if (e === '%') {
-      e = '/';
-    }
-
-    if(this.state.blackBox < 1) {
-      this.setState({
-        blackBox: [e]
-      });
-    } else if (this.state.blackBox.length > 6) {
-      this.setState({
-        blackBox: ['ERROR']
-      });
-      console.log('over 6');
+    if(this.state.error) {
+      console.log('there is an error');
     } else {
-      this.setState({
-        blackBox: [...this.state.blackBox, e]
-      });
+      if(e === 'x') {
+        e = '*'
+      } else if (e === '%') {
+        e = '/';
+      }
 
+      if(this.state.blackBox < 1) {
+        this.setState({
+          blackBox: [e]
+        });
+      } else if (this.state.blackBox.length > 6) {
+        this.setState({
+          blackBox: ['ERROR'],
+          error: true
+        });
+        console.log('over 6');
+      } else {
+        this.setState({
+          blackBox: [...this.state.blackBox, e]
+        });
+
+      }
     }
+
   }
   onEvaluate = () => {
     let result = this.state.blackBox.join('');
@@ -45,13 +52,15 @@ class App extends Component {
   }
   onAllClear = () => {
     this.setState({
-      blackBox: []
+      blackBox: [],
+      error: false
     });
   }
   onClear = () => {
     let pop = this.state.blackBox.slice(0, this.state.blackBox.length - 1);
     this.setState({
-      blackBox: pop
+      blackBox: pop,
+      error: false
     });
   }
   handleDecimal = (e) => {
